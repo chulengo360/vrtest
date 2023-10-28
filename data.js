1eb9465ee720ea014b9067cf1d9a57b83672ff13
chulengo360 <riccardo.tiddi@gmail.com>
2023-10-28 17:26:07 -0300

    Commit

---
+++ data.js
@@ -0,0 +1,153 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20231013_193010_00_049-photoshop",
+      "name": "IMG_20231013_193010_00_049 photoshop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1620965054781003,
+          "pitch": 0.07888005908226958,
+          "rotation": 0,
+          "target": "1-img_20231026_173519_00_merged"
+        },
+        {
+          "yaw": -2.909888921932559,
+          "pitch": 0.06317798939187469,
+          "rotation": 0,
+          "target": "2-img_20231027_134349_00_106_pureshot-hdr_photoshop"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6277897475834795,
+          "pitch": 0.11262918739345729,
+          "title": "Cucina",
+          "text": "Attenti ai fornelli"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20231026_173519_00_merged",
+      "name": "IMG_20231026_173519_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2922572019461387
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1831794602837249,
+          "pitch": 0.0852466039018509,
+          "rotation": 0,
+          "target": "0-img_20231013_193010_00_049-photoshop"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_20231027_134349_00_106_pureshot-hdr_photoshop",
+      "name": "IMG_20231027_134349_00_106_PureShot-HDR_photoshop",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.660976329927209,
+          "pitch": 0.15318115447493064,
+          "rotation": 0,
+          "target": "0-img_20231013_193010_00_049-photoshop"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1412285101958872,
+          "pitch": 0.40691584729430197,
+          "title": "Computer",
+          "text": "Este es mi compu"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
