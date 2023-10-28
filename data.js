982df7ec5d834b9b49cfd743bed3c1146b044a17
chulengo360 <riccardo.tiddi@gmail.com>
2023-10-28 20:08:50 -0300

    Commit

--- data.js
+++ data.js
@@ -1,62 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-img_20231013_193010_00_049-photoshop",
-      "name": "IMG_20231013_193010_00_049 photoshop",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        },
-        {
-          "tileSize": 512,
-          "size": 4096
-        }
-      ],
-      "faceSize": 2992,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": -2.1620965054781003,
-          "pitch": 0.07888005908226958,
-          "rotation": 0,
-          "target": "1-img_20231026_173519_00_merged"
-        },
-        {
-          "yaw": -2.909888921932559,
-          "pitch": 0.06317798939187469,
-          "rotation": 0,
-          "target": "2-img_20231027_134349_00_106_pureshot-hdr_photoshop"
-        }
-      ],
-      "infoHotspots": [
-        {
-          "yaw": 1.6277897475834795,
-          "pitch": 0.11262918739345729,
-          "title": "Cucina",
-          "text": "Attenti ai fornelli"
-        }
-      ]
-    },
-    {
-      "id": "1-img_20231026_173519_00_merged",
+      "id": "0-img_20231026_173519_00_merged",
       "name": "IMG_20231026_173519_00_merged",
       "levels": [
         {
@@ -79,23 +24,30 @@ var APP_DATA = {
       ],
       "faceSize": 1488,
       "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
+        "yaw": 0.019638814774241453,
+        "pitch": 0.062079741313661785,
         "fov": 1.2922572019461387
       },
       "linkHotspots": [
         {
-          "yaw": -0.1831794602837249,
-          "pitch": 0.0852466039018509,
+          "yaw": -0.21145249374031572,
+          "pitch": 0.03552651113668581,
           "rotation": 0,
-          "target": "0-img_20231013_193010_00_049-photoshop"
+          "target": "5-img_20231013_193010_00_049new"
         }
       ],
-      "infoHotspots": []
+      "infoHotspots": [
+        {
+          "yaw": -1.1151109510428867,
+          "pitch": 0.06043461793870719,
+          "title": "Dardo",
+          "text": "Haciendo pruebas con el celu"
+        }
+      ]
     },
     {
-      "id": "2-img_20231027_134349_00_106_pureshot-hdr_photoshop",
-      "name": "IMG_20231027_134349_00_106_PureShot-HDR_photoshop",
+      "id": "1-img_20231027_134414_00_107_pureshot",
+      "name": "IMG_20231027_134414_00_107_PureShot",
       "levels": [
         {
           "tileSize": 256,
@@ -127,18 +79,229 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.660976329927209,
-          "pitch": 0.15318115447493064,
+          "yaw": -1.6792158527384853,
+          "pitch": 0.13226504180447662,
           "rotation": 0,
-          "target": "0-img_20231013_193010_00_049-photoshop"
+          "target": "4-img_20231028_191639_00_119_pureshot"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": 1.1412285101958872,
-          "pitch": 0.40691584729430197,
-          "title": "Computer",
-          "text": "Este es mi compu"
+          "yaw": 1.1911921652176911,
+          "pitch": 0.4173402993643691,
+          "title": "Compu",
+          "text": "Esta es mi compu"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20231028_191406_00_merged",
+      "name": "IMG_20231028_191406_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1325810969614984,
+          "pitch": 0.28399125256806457,
+          "rotation": 0,
+          "target": "3-img_20231028_191448_00_merged"
+        },
+        {
+          "yaw": 1.086957942812516,
+          "pitch": 0.16964001627753866,
+          "rotation": 0,
+          "target": "4-img_20231028_191639_00_119_pureshot"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1593432399366934,
+          "pitch": 0.309759012360292,
+          "title": "Lego",
+          "text": "Estos son los lego de Khates y Amaru"
+        }
+      ]
+    },
+    {
+      "id": "3-img_20231028_191448_00_merged",
+      "name": "IMG_20231028_191448_00_merged",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.305912998470415,
+          "pitch": 0.29046123626505604,
+          "rotation": 0,
+          "target": "2-img_20231028_191406_00_merged"
+        },
+        {
+          "yaw": 1.9400746820637726,
+          "pitch": 0.07697718557017197,
+          "rotation": 0,
+          "target": "4-img_20231028_191639_00_119_pureshot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-img_20231028_191639_00_119_pureshot",
+      "name": "IMG_20231028_191639_00_119_PureShot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.025515993075192256,
+          "pitch": 0.12081189769846468,
+          "rotation": 0,
+          "target": "1-img_20231027_134414_00_107_pureshot"
+        },
+        {
+          "yaw": -2.8283819702550783,
+          "pitch": 0.17841385902216444,
+          "rotation": 0,
+          "target": "2-img_20231028_191406_00_merged"
+        },
+        {
+          "yaw": 3.1153400187231775,
+          "pitch": 0.10663243772269837,
+          "rotation": 0,
+          "target": "5-img_20231013_193010_00_049new"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.616967771616725,
+          "pitch": -0.5674561952445618,
+          "title": "Tambor",
+          "text": "Comprado en Salta"
+        }
+      ]
+    },
+    {
+      "id": "5-img_20231013_193010_00_049new",
+      "name": "IMG_20231013_193010_00_049(new)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2992,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1977553344863097,
+          "pitch": 0.0450026978142084,
+          "rotation": 0,
+          "target": "0-img_20231026_173519_00_merged"
+        },
+        {
+          "yaw": -2.459728833830958,
+          "pitch": 0.20782735650211137,
+          "rotation": 0,
+          "target": "4-img_20231028_191639_00_119_pureshot"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6967691471557318,
+          "pitch": 0.12727414221416922,
+          "title": "Hornallas",
+          "text": "Cuidado con el fuego"
         }
       ]
     }
